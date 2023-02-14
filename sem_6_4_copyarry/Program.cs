e8e1d62f595f441f704960daca207731cc8cba9c
Evgeniy <public-judge@yandex.ru>
2023-02-14 17:34:31 +0300

    метод копирования массива поэлемнтно

---
+++ sem_6_4_copyarry/Program.cs
@@ -0,0 +1,29 @@
+﻿// // *Задача 45: Напишите программу,
+//  которая будет создавать копию заданного массива с помощью поэлементного копирования.*/
+Console.Clear();
+int [] NewArray =GetArray(11,0,10);
+Console.Write($"[{String.Join(", ", NewArray)}]");
+Console.WriteLine("");
+Console.Write($"[{String.Join(", ", CopyArray(NewArray))}]");
+
+
+int [] GetArray (int size, int minValue, int maxValue)
+{
+int [] res = new int [size];
+for (int i=0; i<size; i++)
+{
+    res[i] =new Random().Next(minValue, maxValue);
+}
+return res;
+}
+
+int[] CopyArray(int[] array)
+{
+    int size = (array.Length );
+    int[] result = new int[size];
+    for (int i = 0; i < size ; i++)
+    {        
+        result[i]= array[i];
+    }
+    return result;
+}
