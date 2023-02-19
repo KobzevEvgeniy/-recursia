58cad5e7eceef01e29a73cdfc730eb474a0422e3
Evgeniy <public-judge@yandex.ru>
2023-02-19 13:28:58 +0300

    Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,и возвращает значение этого элемента или же указание, что такого элемента нет.

---
+++ task_50_HomeWork/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 100);
+PrintArray(array);
+Write("Введитe строку элемента: ");
+int position1 = int.Parse(ReadLine());
+Write("Введитe столбец элемента: ");
+int position2 = int.Parse(ReadLine());
+ChekPosition(array, position1,position2,rows,columns);
+
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void ChekPosition(int [,]array,int position1, int position2, int rows, int columns)
+{    
+  if (position1 < 0 | position1 > array.GetLength(0) - 1 | position2 < 0 | position2 > array.GetLength(1) - 1)
+    {
+        Console.WriteLine("Элемент не существует");
+    }
+    else
+    {
+        Console.WriteLine($"Значение элемента массива =  {array[position1, position2]}");
+    }
+}
+ 
+        
